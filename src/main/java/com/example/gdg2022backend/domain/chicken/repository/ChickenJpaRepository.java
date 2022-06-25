a0e896f2b88a0fc1f2223218536bc716e3e4d473
hyune <choi8608@gmail.com>
2022-06-26 03:56:56 +0900

    ChickenJpaRepository 수정

--- src/main/java/com/example/gdg2022backend/domain/chicken/repository/ChickenJpaRepository.java
+++ src/main/java/com/example/gdg2022backend/domain/chicken/repository/ChickenJpaRepository.java
@@ -2,40 +2,42 @@ package com.example.gdg2022backend.domain.chicken.repository;
 
 import com.example.gdg2022backend.domain.analysis.service.dto.MatchingChickenResult;
 import com.example.gdg2022backend.domain.analysis.service.dto.SurveyResult;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class ChickenJpaRepository {
-    private final EntityManager em;
 
-    public List<MatchingChickenResult> matchingChicken(SurveyResult surveyResult) {
-        String sql = "(select chicken_id as chicken_id, brand as brand, name as name, description as description, sum as score from (\n" +
-                "select chicken_id, chicken.brand, chicken.name, chicken.description,\n" +
-                "       (chicken.a1 + chicken.a2 + chicken.a3 + chicken.b1 + chicken.b2 + chicken.b3 + chicken.b4 +\n" +
-                "       chicken.c1 + chicken.c2 + chicken.c3 + chicken.c4 + chicken.d1 + chicken.d2) as sum\n" +
-                "from chicken) as data1\n" +
-                "where sum > ?\n" +
-                "order by sum asc\n" +
-                "limit 5)\n" +
-                "UNION\n" +
-                "(select chicken_id as chicken_id, brand as brand, name as name, description as description, sum as score from (\n" +
-                "select chicken_id, chicken.brand, chicken.name, chicken.description,\n" +
-                "       (chicken.a1 + chicken.a2 + chicken.a3 + chicken.b1 + chicken.b2 + chicken.b3 + chicken.b4 +\n" +
-                "       chicken.c1 + chicken.c2 + chicken.c3 + chicken.c4 + chicken.d1 + chicken.d2) as sum\n" +
-                "from chicken) as data1\n" +
-                "where sum < ?\n" +
-                "order by sum desc\n" +
-                "limit 5)";
+	private final EntityManager em;
 
-        Query nativeQuery = em.createNativeQuery(sql).setParameter(surveyResult.sum(), surveyResult.sum());
-        return nativeQuery.getResultList();
-    }
+	public List<MatchingChickenResult> matchingChicken(SurveyResult surveyResult) {
+		String sql = "(select chicken_id as chicken_id, brand as brand, name as name, description as description, sum as score from ( " +
+				"select chicken_id, chicken.brand, chicken.name, chicken.description, " +
+				"       (chicken.a1 + chicken.a2 + chicken.a3 + chicken.b1 + chicken.b2 + chicken.b3 + chicken.b4 + " +
+				"       chicken.c1 + chicken.c2 + chicken.c3 + chicken.c4 + chicken.d1 + chicken.d2) as sum " +
+				"from chicken) as data1 " +
+				"where sum > ?1 " +
+				"order by sum asc " +
+				"limit 5) " +
+				"UNION " +
+				"(select chicken_id as chicken_id, brand as brand, name as name, description as description, sum as score from ( " +
+				"select chicken_id, chicken.brand, chicken.name, chicken.description, " +
+				"       (chicken.a1 + chicken.a2 + chicken.a3 + chicken.b1 + chicken.b2 + chicken.b3 + chicken.b4 + " +
+				"       chicken.c1 + chicken.c2 + chicken.c3 + chicken.c4 + chicken.d1 + chicken.d2) as sum " +
+				"from chicken) as data1 " +
+				"where sum < ?2 " +
+				"order by sum desc " +
+				"limit 5)";
+
+		Query nativeQuery = em.createNativeQuery(sql)
+				.setParameter(1, surveyResult.sum())
+				.setParameter(2, surveyResult.sum());
+		return nativeQuery.getResultList();
+	}
 }
 
 
