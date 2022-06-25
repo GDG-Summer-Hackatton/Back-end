757d6117fdcf71adb5c0e2f661ddf53ac0058a44
hyune <choi8608@gmail.com>
2022-06-26 02:41:38 +0900

    AnalysisService 코드 정리

--- src/main/java/com/example/gdg2022backend/domain/analysis/service/AnalysisService.java
+++ src/main/java/com/example/gdg2022backend/domain/analysis/service/AnalysisService.java
@@ -1,7 +1,6 @@
 package com.example.gdg2022backend.domain.analysis.service;
 
 import com.example.gdg2022backend.domain.analysis.service.dto.SurveyResult;
-import com.example.gdg2022backend.domain.chicken.repository.ChickenRepository;
 import com.example.gdg2022backend.domain.survey.entity.Survey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,9 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AnalysisService {
 
-	private final ChickenRepository chickenRepository;
-
-	// TODO surveyResult 생성 로직
 	public SurveyResult analysis(final Survey survey) {
 		return new SurveyResult(survey);
 	}
