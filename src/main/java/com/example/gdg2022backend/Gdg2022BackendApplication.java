64fdae91fa0af746c6a286f59cf6eca37b50401a
hyune <choi8608@gmail.com>
2022-06-25 14:38:46 +0900

    init

---
+++ src/main/java/com/example/gdg2022backend/Gdg2022BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdg2022backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdg2022BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdg2022BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gdg2022backend/Gdg2022BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdg2022backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdg2022BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
